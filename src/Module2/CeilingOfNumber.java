a31317a8b2504a70579056a3b1ad16a70cbbf271
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2022-10-23 13:02:31 +0530

    Same Pattern Problem Based on Binary Search

---
+++ src/Module2/CeilingOfNumber.java
@@ -0,0 +1,36 @@
+package Module2;
+
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] arr = {-18, -10, 0, 1,2, 5, 15, 48, 78, };
+        int target = 79;
+        System.out.println(cieling(arr, target));
+    }
+
+    static int cieling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > arr[mid]){
+                start = mid+1;
+            }
+            else if (target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target == arr[mid]){
+                return mid;
+            }
+            if (target > arr[arr.length-1]){
+                return -1;
+            }
+        }
+        // the next Greater Element and Smallest of that rnage is where start points when target is not found
+        // this is Ceiling of Number
+
+        return start;
+    }
+}
---
+++ src/Module2/FloorOfNumber.java
@@ -0,0 +1,32 @@
+package Module2;
+
+public class FloorOfNumber {
+    public static void main(String[] args) {
+        int[] arr = {-18, -10, 0, 1,2, 5, 15, 48, 78, };
+        int target = 6;
+        System.out.println(floorNumber(arr, target));
+    }
+
+    static int floorNumber(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > arr[mid]){
+                start = mid+1;
+            }
+            if (target < arr[mid]){
+                end = mid-1;
+            }
+            if(target == arr[mid]){
+                return mid;
+            }
+        }
+        // the bigger element smaller than
+        // this is Ceiling of Number
+
+        return end;
+    }
+}
